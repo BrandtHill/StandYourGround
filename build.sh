8bbcc4724fe2cd959e3631dddc5efe96d720e9c3
Brandt Hill <brandtlarsonhill@gmail.com>
2021-02-21 20:54:35 -0600

    build script

--- build.sh
+++ build.sh
@@ -2,4 +2,8 @@
 
 ant -Dant.build.javac.target=1.8 -Dant.build.javac.source=1.8
 
-zip -r SYG.zip ./out/
+zip -r SYG_Windows.zip ./out/SYG.jar ./out/res ./out/*.dll
+
+zip -r SYG_Linux.zip ./out/SYG.jar ./out/res ./out/*.so
+
+zip -r SYG_macOS.zip ./out/SYG.jar ./out/res ./out/*.jnilib ./out/*.dylib
